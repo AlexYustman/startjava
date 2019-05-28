c64c3444b01cb8459a654a4d9beb126a1c502501
alex.yustman <ya.azeryusifov@ya.ru>
2019-05-28 14:56:20 +0300

    добавлено форматирование

--- Lesson 1/Variable.java
+++ Lesson 1/Variable.java
@@ -1,20 +1,20 @@
 public class Variable {
 	public static void main(String[] args) {
-	  byte computerQuantity=1;
-	  System.out.println("Kolichestvo kompjyuterov: "+computerQuantity);
-	    short sinceWhere=2004;
-	    System.out.println("Kogda poyavilsya pervyi PC: "+sinceWhere);
-          int core=4;
-          System.out.println("Skolko yader u CPU: "+core);
-            long ram=4294967;
-            System.out.println("Operativnaya pamyat' v Mhz: "+ram);
-              float cpuFrequency=1.123456f;
-              System.out.println("Chastota CPU v Ghz: "+cpuFrequency);
-                double pi=3.141592653589793238462643;
-                System.out.println("Chislo Pi: "+pi);
-                  char symbol='X';
-                  System.out.println("Prosto simvol: "+symbol);
-                    boolean yaas=true;
-                    System.out.println("Ustal pisat' kod: "+yaas);
-}
+		byte computerQuantity=1;
+		System.out.println("Kolichestvo kompjyuterov: "+computerQuantity);
+		short sinceWhere=2004;
+		System.out.println("Kogda poyavilsya pervyi PC: "+sinceWhere);
+		int core=4;
+		System.out.println("Skolko yader u CPU: "+core);
+		long ram=4294967;
+		System.out.println("Operativnaya pamyat' v Mhz: "+ram);
+		float cpuFrequency=1.123456f;
+		System.out.println("Chastota CPU v Ghz: "+cpuFrequency);
+		double pi=3.141592653589793238462643;
+		System.out.println("Chislo Pi: "+pi);
+		char symbol='X';
+		System.out.println("Prosto simvol: "+symbol);
+		boolean yaas=true;
+		System.out.println("Ustal pisat' kod: "+yaas);
+	}
 }
