3de1709c74d4cb1e7d73137fead17532d158c240
alex.yustman <ya.azeryusifov@ya.ru>
2019-05-30 12:00:47 +0300

    Исправлено форматирование кода

--- Lesson 1/ConditionalStatement.java
+++ Lesson 1/ConditionalStatement.java
@@ -1,34 +1,30 @@
 public class ConditionalStatement {
-	public static void main (String [] args){
+	public static void main (String [] args) {
 		int i = 32;
-		if(i>20){
-			System.out.println("Вам больше 20 лет. Точнее, вам "+i+" года");
+		if (i > 20) {
+			System.out.println("Вам больше 20 лет. Точнее, вам " + i + " года");
 		}
 		
 		boolean male = true;
-		if(male==true){
-			System.out.println("Вы - мужчина и это "+male);
-		}
-		else if(male!=true) {
+		if (male) {
+			System.out.println("Вы - мужчина и это " + male);
+		} else if (!male) {
 			System.out.println("Вы - женщина");
 		}
 		
 		double height = 1.65d;
-		if(height<1.80){
-			System.out.println("У вас средний рост, равный "+height);
-		}
-		else{
+		if (height < 1.80) {
+			System.out.println("У вас средний рост, равный " + height + "м");
+		} else {
 			System.out.println("У вас высокий рост");
 		}
 
 		char firstLetter = 'A';
-		if(firstLetter=='M'){
+		if (firstLetter == 'M') {
 			System.out.println("Ваше имя Mлексей");
-		}
-		else if(firstLetter=='I'){
+		} else if (firstLetter == 'I') {
 			System.out.println("Ваше имя Iлексей");
-		}
-		else{
+		} else {
 			System.out.println("Ваше имя Алексей");
 		}
 	}
