4379b8dc1a66bc8d776d6682ebe99dd352d93d49
alex.yustman <ya.azeryusifov@ya.ru>
2019-05-31 13:07:34 +0300

    Исправлены недочёты по синтаксису и форматированию

--- Lesson 1/Cycle.java
+++ Lesson 1/Cycle.java
@@ -1,24 +1,24 @@
 public class Cycle {	
     public static void main(String[] args) {
-        for (int a = 0; a <= 20; a++) {
-        	 System.out.print(a + " ");
+        for (int j = 0; j <= 20; j++) {
+        	 System.out.print(j + " ");
         }
         
-        int b = 6;
-        while (b >= -6) {
-        	System.out.println(b + " ");
-        	b-=2;
+        int k = 6;
+        while (k >= -6) {
+        	System.out.println(k);
+        	k -= 2;
         }
 
         int sum = 0;
         int i = 10;
         do {
             if (i % 2 == 1) {
-                sum = sum + i;
+                sum += i;
             }
             i++;
         } while (i <= 20);
 
-        System.out.println("11 + 13 + 15 + 17 + 19 = " + sum);
+        System.out.println("Сумма всех нечётных чисел в интервале от 10 до 20 равна: " + sum);
     }
 }
