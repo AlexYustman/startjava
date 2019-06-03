2335d7722966792904251be4cf3ffc8cb5b11b7b
alex.yustman <ya.azeryusifov@ya.ru>
2019-06-03 21:19:09 +0300

    Исправлено решение задачи

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -2,30 +2,24 @@ public class Calculator {
     public static void main(String[] args) {
     	int a = 25;
     	int b = 12;
-    	int sum = a + b;
-    	int difference = a - b;
-    	int product = a * b;
-    	double quotient = a / b;
-    	long exponentiation = 1; /* возведение в степень*/
-
+    	char symbol = '*';
     	System.out.println("Введите два целых положительных числа и знак операции (+, -, *, /, ^, %)");
-    	if (sum == a + b) {
+    	if (symbol == '+') {
     		System.out.println("Вы ввели " + a + "+" + b + ", где ответ равен " + (a + b));
-    	} 
-    	if (difference == a - b) {
+    	} else if (symbol == '-') {
     		System.out.println("Вы ввели " + a + "-" + b + ", где ответ равен " + (a - b));
-    	} 
-    	if (product == a * b) {
+    	} else if (symbol == '*') {
     		System.out.println("Вы ввели " + a + "*" + b + ", где ответ равен " + (a * b));
-    	}
-    	if (quotient == a / b) {
+    	} else if (symbol == '/') {
     		System.out.println("Вы ввели " + a + "/" + b + ", где ответ равен " + (a / b));
+    	} else if (symbol == '^') {
+    		for (int i = 1; i <= b; i++) {
+    		a *= a;
+    		System.out.println("Вы ввели " + a + "^" + b + ", где ответ равен " + a);
     	}
-    	for (int i = 1; i <= b; i++) {
-    		exponentiation *= a;
-    	} 
-    	System.out.println("Вы ввели " + a + "^" + b + ", где ответ равен " + exponentiation);
-    	System.out.println("Остаток от деления " + a + " на " + b + " равен " + (a % b));
-
+    	} else if (symbol == '%') {
+    		System.out.println("Остаток от деления " + a + " на " + b + " равен " + (a % b));
+    	}
+    	
     }
 } 
