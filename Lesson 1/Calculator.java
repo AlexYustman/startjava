48662d6af3c8a60403b5fdddc5a65b58bf5a4014
alex.yustman <ya.azeryusifov@ya.ru>
2019-06-04 20:49:56 +0300

    Добавлено решение для возведения в нулевую степень

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -3,6 +3,7 @@ public class Calculator {
     	int a = 25;
     	int b = 12;
     	char symbol = '*';
+
     	System.out.println("Введите два целых положительных числа и знак операции (+, -, *, /, ^, %)");
     	if (symbol == '+') {
     		System.out.println("Вы ввели " + a + "+" + b + ", где ответ равен " + (a + b));
@@ -13,13 +14,15 @@ public class Calculator {
     	} else if (symbol == '/') {
     		System.out.println("Вы ввели " + a + "/" + b + ", где ответ равен " + (a / b));
     	} else if (symbol == '^') {
+    		if (b == 0) {
+    			System.out.println("Вы ввели " + a + "^" + b + ", где ответ равен " + 1);
+    		}
     		for (int i = 1; i <= b; i++) {
-    		a *= a;
-    		System.out.println("Вы ввели " + a + "^" + b + ", где ответ равен " + a);
+    			a *= a; 
+    			System.out.println("Вы ввели " + a + "^" + b + ", где ответ равен " + a);
     	}
     	} else if (symbol == '%') {
     		System.out.println("Остаток от деления " + a + " на " + b + " равен " + (a % b));
     	}
-    	
     }
 } 
