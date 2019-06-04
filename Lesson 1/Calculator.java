c492258cb8f7136c679933d0617a2d7b857922e2
alex.yustman <ya.azeryusifov@ya.ru>
2019-06-04 22:48:10 +0300

    Переименована переменная char. Форматирование.

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -2,26 +2,26 @@ public class Calculator {
     public static void main(String[] args) {
     	int a = 25;
     	int b = 12;
-    	char symbol = '*';
+    	char mathOperation = '*';
 
     	System.out.println("Введите два целых положительных числа и знак операции (+, -, *, /, ^, %)");
-    	if (symbol == '+') {
+    	if (mathOperation == '+') {
     		System.out.println("Вы ввели " + a + "+" + b + ", где ответ равен " + (a + b));
-    	} else if (symbol == '-') {
+    	} else if (mathOperation == '-') {
     		System.out.println("Вы ввели " + a + "-" + b + ", где ответ равен " + (a - b));
-    	} else if (symbol == '*') {
+    	} else if (mathOperation == '*') {
     		System.out.println("Вы ввели " + a + "*" + b + ", где ответ равен " + (a * b));
-    	} else if (symbol == '/') {
+    	} else if (mathOperation == '/') {
     		System.out.println("Вы ввели " + a + "/" + b + ", где ответ равен " + (a / b));
-    	} else if (symbol == '^') {
+    	} else if (mathOperation == '^') {
     		if (b == 0) {
     			System.out.println("Вы ввели " + a + "^" + b + ", где ответ равен " + 1);
     		}
     		for (int i = 1; i <= b; i++) {
     			a *= a; 
     			System.out.println("Вы ввели " + a + "^" + b + ", где ответ равен " + a);
-    	}
-    	} else if (symbol == '%') {
+    		}
+    	} else if (mathOperation == '%') {
     		System.out.println("Остаток от деления " + a + " на " + b + " равен " + (a % b));
     	}
     }
