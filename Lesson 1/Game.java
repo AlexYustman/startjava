0ca5153f639190a92bd0f797d739b298dd5161c9
alex.yustman <ya.azeryusifov@ya.ru>
2019-06-11 20:09:03 +0300

    Исправлены названия переменных и оператор if

--- Lesson 1/Game.java
+++ Lesson 1/Game.java
@@ -1,19 +1,19 @@
 public class Game {
 	public static void main(String[] args) {
-		int computersNumber = 39;
-		int usersNumber = 99;
+		int computerNumber = 39;
+		int userNumber = 99;
 
 		System.out.println("Угадайте целое число, загаданное компьютером в интервале от 0 до 100");
-		while (usersNumber != computersNumber) {
-			if (usersNumber > computersNumber) {
+		while (userNumber != computerNumber) {
+			if (userNumber > computerNumber) {
 				System.out.println("Введённое вами число больше загаданного компьютером. Попробуйте ещё");
-				usersNumber--;
+				userNumber--;
 			}
-			if (usersNumber < computersNumber) {
+			else if (userNumber < computerNumber) {
 				System.out.println("Введённое вами число меньше загаданного компьютером. Попробуйте ещё");
-				usersNumber++;
+				userNumber++;
 			}
 		}
-		System.out.println("Вы угадали! Введённое вами " + usersNumber + " равно загаданному " + computersNumber);
+		System.out.println("Вы угадали! Введённое вами " + userNumber + " равно загаданному " + computerNumber);
 	}
 }
